5544c463efc616647cb2a7b5095ba7f077807be3
David Napravnik <ebrithilnogare@gmail.com>
2024-08-30 18:45:17 +0200

    added more UI to laptop screen

---
+++ Assets/Scripts/DesktopManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DesktopManager : MonoBehaviour
+{
+    public void AddIconOnDesktop(GameObject icon)
+    {
+        icon.SetActive(true);
+    }
+    public void RemoveIconFromDesktop(GameObject icon)
+    {
+        icon.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/WindowManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour
+{
+    public void CloseWindow(GameObject window)
+    {
+        window.SetActive(false);
+    }
+    public void OpenWindow(GameObject window)
+    {
+        window.SetActive(true);
+    }
+}
