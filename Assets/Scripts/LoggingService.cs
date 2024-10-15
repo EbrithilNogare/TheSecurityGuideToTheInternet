006b086e52ba2c3881d02bead4c3d6af77237b49
David Napravnik <ebrithilnogare@gmail.com>
2024-10-15 12:10:24 +0200

    made http over unity instead of system

--- Assets/Scripts/LoggingService.cs
+++ Assets/Scripts/LoggingService.cs
@@ -1,16 +1,14 @@
-using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public static class LoggingService
 {
     private const string NewRelicLogUrl = "https://log-api.eu.newrelic.com/log/v1?Api-Key=";
     private static string ApiKey;
-    private static HttpClient client;
 
     private static string sessionId;
     private static Environment environment;
@@ -22,9 +20,9 @@ public static class LoggingService
 
     static LoggingService()
     {
-        ApiKey = Encoding.UTF8.GetString(Convert.FromBase64String("ZXUwMXh4MDlkNWJjMWJiZmIzYjAxNDA2NGM3ZmMyMDNGRkZGTlJBTA=="));
-        client = new HttpClient();
-        sessionId = Guid.NewGuid().ToString();
+        ApiKey = Encoding.UTF8.GetString(System.Convert.FromBase64String("ZXUwMXh4MDlkNWJjMWJiZmIzYjAxNDA2NGM3ZmMyMDNGRkZGTlJBTA==")); // key is visible in request anyway
+        sessionId = System.Guid.NewGuid().ToString();
+
         permissionToLog = true; // todo make it false and ask for it
 
 #if UNITY_EDITOR
@@ -37,7 +35,7 @@ public static class LoggingService
         {
             if (type == LogType.Error || type == LogType.Exception)
             {
-                _ = SendLogsAsync(LogLevel.ERROR, LogCategory.ERROR, message, new Dictionary<string, string> { { "stackTrace", stackTrace } });
+                SendLogsAsync(LogLevel.ERROR, LogCategory.ERROR, message, new Dictionary<string, string> { { "stackTrace", stackTrace } });
             }
         };
 
@@ -48,7 +46,7 @@ public static class LoggingService
         return message.Replace("'", "\"").Replace("\"", "\\\"");
     }
 
-    private static async Task SendLogsAsync(LogLevel logLevel, LogCategory category, string description, Dictionary<string, string> attributes = null)
+    private static async void SendLogsAsync(LogLevel logLevel, LogCategory category, string description, Dictionary<string, string> attributes = null)
     {
         if (!permissionToLog) { return; }
 
@@ -81,18 +79,24 @@ public static class LoggingService
     ""attributes"": {{ {attributesJson.ToString()} }}
 }}";
 
-        var content = new StringContent(logData, Encoding.UTF8, "application/json");
+        using (UnityWebRequest request = new UnityWebRequest(NewRelicLogUrl + ApiKey, "POST"))
+        {
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(logData);
+            request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
 
-        var response = await client.PostAsync(NewRelicLogUrl + ApiKey, content);
-        response.EnsureSuccessStatusCode();
-        //Debug.Log("Logs: " + response.StatusCode);
+            var operation = request.SendWebRequest();
+
+            while (!operation.isDone)
+                await Task.Yield();
+        }
     }
+
     public static void GrandPermissionToTrack(bool permission) { permissionToLog = permission; }
 
     public static void Log(LogCategory category, string description)
     {
-        _ = SendLogsAsync(LogLevel.INFO, category, description);
+        SendLogsAsync(LogLevel.INFO, category, description);
     }
-
-
 }
