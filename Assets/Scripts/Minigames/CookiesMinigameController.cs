8fbaf3224dd616a12b95d26845d1890f3b51cbc8
David Napravnik <ebrithilnogare@gmail.com>
2025-03-04 20:30:26 +0100

    added  dragin mechanic to cookie minigame

--- Assets/Scripts/Minigames/CookiesMinigameController.cs
+++ Assets/Scripts/Minigames/CookiesMinigameController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum CookieIngredient
+{
+    Chocolate1, Chocolate2, Chocolate3,
+    Egg1, Egg2, Egg3,
+    Flour1, Flour2, Flour3,
+    Milk1, Milk2, Milk3,
+}
 
 public class CookiesMinigameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<CookiesMinigameDragable> dragableObjects;
+    public GameObject dropZone;
+    public GameObject draggingParent;
+    public List<Image> cookieScoreImages;
+
+    List<CookiesMinigameDragable> cookieIngredientsInBowl = new List<CookiesMinigameDragable>();
+    int currentCookieCategoryIndex = 0;
+
+
+    public void CookieAddedToBowl(CookiesMinigameDragable cookieIngredient)
     {
-        
+        Debug.Log("CookieAddedToBowl: " + cookieIngredient);
+        cookieIngredientsInBowl.Add(cookieIngredient);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartAgain()
     {
-        
+        foreach (var item in cookieIngredientsInBowl)
+        {
+            item.Reset();
+        }
+
+        cookieIngredientsInBowl.Clear();
+
+        // todo reset other things
+    }
+
+    public void FinishAndBake()
+    {
+        // todo finish and bake
+
+        currentCookieCategoryIndex++;
+        cookieIngredientsInBowl.Clear();
+
+        if (currentCookieCategoryIndex < cookieScoreImages.Count)
+        {
+            // todo evaluate score
+        }
     }
 }
---
+++ Assets/Scripts/Minigames/CookiesMinigameDragable.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CookiesMinigameDragable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public CookieIngredient cookieIngredient;
+
+    Canvas canvas;
+    CanvasGroup canvasGroup;
+    RectTransform rectTransform;
+    CookiesMinigameController cookiesMinigameController;
+    RectTransform dropZone;
+    Transform draggingParent;
+    Vector2 originalPosition;
+    Transform originalParent;
+
+    void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        canvasGroup = GetComponent<CanvasGroup>() != null ? GetComponent<CanvasGroup>() : gameObject.AddComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
+        cookiesMinigameController = FindObjectOfType<CookiesMinigameController>();
+        dropZone = cookiesMinigameController.dropZone.GetComponent<RectTransform>();
+        draggingParent = cookiesMinigameController.draggingParent.transform;
+        originalParent = rectTransform.parent;
+    }
+
+    public void Reset()
+    {
+        rectTransform.localPosition = originalPosition;
+        transform.SetParent(originalParent);
+        gameObject.SetActive(true);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.8f;
+        originalPosition = rectTransform.localPosition;
+        rectTransform.SetParent(draggingParent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / (new Vector2(rectTransform.lossyScale.x, rectTransform.lossyScale.y));
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.alpha = 1f;
+        transform.SetParent(originalParent);
+        EvaluateDrop();
+    }
+
+    private void EvaluateDrop()
+    {
+        if (RectTransformUtility.RectangleContainsScreenPoint(dropZone, Input.mousePosition, canvas.worldCamera))
+        {
+            // droped in drop zone
+            rectTransform.localPosition = originalPosition;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            // droped somewhere else
+            rectTransform.localPosition = originalPosition;
+        }
+
+        // send information to controller
+        cookiesMinigameController.CookieAddedToBowl(this);
+    }
+}
