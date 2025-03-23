d2d8ada1c10dcaeb3682c88d89bc4b1eb782ed04
David Napravnik <ebrithilnogare@gmail.com>
2025-03-23 14:29:02 +0100

    added tutorials

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
+    public string minigameName;
+    public GameObject tutorialDialog;
 
+    public void ShowTutorialDialog() {
+        LoggingService.Log(LoggingService.LogCategory.Minigame, "Tutorial dialog opened in minigame: " + minigameName);
+        tutorialDialog.SetActive(true);
+    }
 
-
-
-    void Start() {
-
+    public void CloseTutorialDialog() {
+        LoggingService.Log(LoggingService.LogCategory.Minigame, "Tutorial dialog closed in minigame: " + minigameName);
+        tutorialDialog.SetActive(false);
     }
 }
